b304a8f30ff8b1c206689672d349b25fbdf88278
Haitam Borqane <haytamborquane@gmail.com>
2024-02-27 22:46:21 +0100

    english text processing

--- cleaning/text_processing.py
+++ cleaning/text_processing.py
@@ -3,6 +3,7 @@ import re
 import string
 import nltk
 from nltk.corpus import stopwords
+from typing import List
 
 def get_data_per_language(df: pd.core.frame.DataFrame,
                         language: str) -> pd.core.frame.DataFrame:
@@ -126,3 +127,64 @@ def get_contractions(contractions_dict):
 def get_eng_letters():
     eng_letters = list(string.ascii_lowercase)
     return eng_letters
+
+def get_contractions_dict():
+    # Dictionary of English Contractions
+    contractions_dict = { "ain't": "are not","'s":" is","aren't": "are not",
+                        "can't": "cannot","can't've": "cannot have",
+                        "'cause": "because","could've": "could have","couldn't": "could not",
+                        "couldn't've": "could not have", "didn't": "did not","doesn't": "does not",
+                        "don't": "do not","hadn't": "had not","hadn't've": "had not have",
+                        "hasn't": "has not","haven't": "have not","he'd": "he would",
+                        "he'd've": "he would have","he'll": "he will", "he'll've": "he will have",
+                        "how'd": "how did","how'd'y": "how do you","how'll": "how will",
+                        "I'd": "I would", "I'd've": "I would have","I'll": "I will",
+                        "I'll've": "I will have","I'm": "I am","I've": "I have", "isn't": "is not",
+                        "it'd": "it would","it'd've": "it would have","it'll": "it will",
+                        "it'll've": "it will have", "let's": "let us","ma'am": "madam",
+                        "mayn't": "may not","might've": "might have","mightn't": "might not",
+                        "mightn't've": "might not have","must've": "must have","mustn't": "must not",
+                        "mustn't've": "must not have", "needn't": "need not",
+                        "needn't've": "need not have","o'clock": "of the clock","oughtn't": "ought not",
+                        "oughtn't've": "ought not have","shan't": "shall not","sha'n't": "shall not",
+                        "shan't've": "shall not have","she'd": "she would","she'd've": "she would have",
+                        "she'll": "she will", "she'll've": "she will have","should've": "should have",
+                        "shouldn't": "should not", "shouldn't've": "should not have","so've": "so have",
+                        "that'd": "that would","that'd've": "that would have", "there'd": "there would",
+                        "there'd've": "there would have", "they'd": "they would",
+                        "they'd've": "they would have","they'll": "they will",
+                        "they'll've": "they will have", "they're": "they are","they've": "they have",
+                        "to've": "to have","wasn't": "was not","we'd": "we would",
+                        "we'd've": "we would have","we'll": "we will","we'll've": "we will have",
+                        "we're": "we are","we've": "we have", "weren't": "were not","what'll": "what will",
+                        "what'll've": "what will have","what're": "what are", "what've": "what have",
+                        "when've": "when have","where'd": "where did", "where've": "where have",
+                        "who'll": "who will","who'll've": "who will have","who've": "who have",
+                        "why've": "why have","will've": "will have","won't": "will not",
+                        "won't've": "will not have", "would've": "would have","wouldn't": "would not",
+                        "wouldn't've": "would not have","y'all": "you all", "y'all'd": "you all would",
+                        "y'all'd've": "you all would have","y'all're": "you all are",
+                        "y'all've": "you all have", "you'd": "you would","you'd've": "you would have",
+                        "you'll": "you will","you'll've": "you will have", "you're": "you are",
+                        "you've": "you have"}
+    return contractions_dict
+
+def replace(match):
+    contractions_dict = get_contractions_dict()
+    return contractions_dict[match.group(0)]
+
+# Function for expanding contractions
+def expand_contractions(text: str, contractions_re):
+    return contractions_re.sub(replace, text)
+
+def remove_eng_stop_words(text, eng_stop_words):
+    text = ' '.join(word for word in text.split() if word not in eng_stop_words)
+    return text
+
+def preprocess_english_text(df: pd.core.frame.DataFrame,
+                            eng_stop_words: List[str]):
+    english_df = get_data_per_language(df, language="eng")
+    english_df['content'] = english_df['content'].apply(lambda x: expand_contractions(x))
+    english_df['content'] = english_df['content'].apply(lambda x: remove_eng_stop_words(x, eng_stop_words))
+    english_df = english_df.reset_index(drop = True)
+    return english_df
