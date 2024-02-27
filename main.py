07e2f9b70c3c24b925c08f14fc401a312312b92d
Haitam Borqane <haytamborquane@gmail.com>
2024-02-27 23:14:16 +0100

    implementing all the text processing function onto the main script

--- main.py
+++ main.py
@@ -14,7 +14,15 @@ from cleaning.data_cleaning import (fix_datetime_column,
                                     reformat_sentiment_col,
                                     filter_columns,
                                     compute_sentiment_col)
-from cleaning.text_processing import (separate_text_by_language)
+from cleaning.text_processing import (get_contractions, 
+                                        separate_text_by_language,
+                                        preprocess_all_text,
+                                        preprocess_arabic_text,
+                                        preprocess_english_text,
+                                        get_ara_stopwords,
+                                        get_punctuations,
+                                        get_eng_stopwords,
+                                        get_contractions_dict)
 
 location_name_to_city = {
     # Mapper of some common locations with their cities
@@ -87,3 +95,24 @@ if __name__ == "__main__":
     # Text Processing
     # Fixing Languages
     correct_language_rating_data = separate_text_by_language(main_rating_data)
+    
+    # Process all text
+    text_processed_data = preprocess_all_text(correct_language_rating_data)
+    
+    # Process arabic text
+    punctuations = get_punctuations()
+    arab_stop_words = get_ara_stopwords()
+    arabic_processed_data = preprocess_arabic_text(text_processed_data,
+                                                    punctuations,
+                                                    arab_stop_words)
+    
+    # Process english text
+    eng_stop_words = get_eng_stopwords()
+    contractions_dict = get_contractions_dict()
+    contractions_re = get_contractions(contractions_dict)
+    english_processed_data = preprocess_english_text(text_processed_data,
+                                                    eng_stop_words,
+                                                    contractions_re)
+    
+    # Concat all data
+    full_processed_data = pd.concat([arabic_processed_data, english_processed_data]).reset_index(drop=True)
